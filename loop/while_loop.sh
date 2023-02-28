3425ac7876885a879712d622ad78ea3b32cde33e
khadiza21 <bibikhadiza474@gmail.com>
2023-02-28 21:23:41 +0600

    while loop

--- loop/while_loop.sh
+++ loop/while_loop.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #output 1 to 5
-echo "while 01: "
+echo "loop 01: "
 # set n=1
 n=1
 while [ $n -le 5 ]
@@ -12,17 +12,31 @@ done
 echo $lines
 echo $lines
 
-echo "while 02: "
+
+
+echo "loop 02: "
 n=1
-while (( $n <= 5 ))
+while (( $n <= 5 )) 
 do
 echo "Welcome $n time!"
-n=$ (( n+1 ))
+n=$(( n+1 ))
 done
+echo $lines
+echo $lines
 
 
-
-
+echo "loop 03: "
+str_input=hello
+while [ "$str_input" != "bye" ]
+do
+echo  "Please type something in (bye to quit)" 
+read  str_input
+echo $lines
+echo "You typed: $str_input"
+break
+done
+echo $lines
+echo $lines
 
 
 
