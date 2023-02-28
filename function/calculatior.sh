33a3c1dd2acde6729c528f4df2d19a4c04abf85c
khadiza21 <bibikhadiza474@gmail.com>
2023-03-01 00:09:10 +0600

    calculator function

---
+++ function/calculatior.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+# function to add three numbers
+add_three_numbers() {
+  echo "Please enter the first number:"
+  read num1
+  
+  echo "Please enter the second number:"
+  read num2
+  
+  echo "Please enter the third number:"
+  read num3
+  
+  sum=$((num1 + num2 + num3))
+  echo "Addition is $sum"
+  echo $lines
+  
+  return $sum
+}
+
+# function to multiply two numbers
+multiply_two_numbers() {
+  echo "Please enter the first number:"
+  read num1
+  
+  echo "Please enter the second number:"
+  read num2
+  
+  product=$((num1 * num2))
+  echo "Multipling is $product"
+  echo $lines
+  
+  return $product
+}
+
+# function to add two values
+addition() {
+  echo "Add three numbers: "
+  add_three_numbers
+  sum1=$? #capturing the output of sum
+  echo $lines
+  echo $lines
+  
+  echo "Multipling two nubmers: "
+  multiply_two_numbers
+  product=$? #capturing the output of sum
+  echo $lines
+  echo $lines
+ 
+  total=$((sum1 + product))
+  
+  echo "The result of addition is: $total"
+  echo $lines
+  echo $lines
+}
+
+addition
